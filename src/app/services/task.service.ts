6e7a7c2a5f501032db95f5f873c624a6671850b2
diogenes-omar-lora <omarlora08@hotmail.com>
2022-11-08 09:46:23 -0400

    no se

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -10,27 +10,15 @@ tasks: Task[];
       {title:'Welcome',description:'This is My Project', hide:true}
     ];
    }
+   
    gettasks(){
     return this.tasks;
-    // if(localStorage.getItem('task') === null){
-    //   return this.tasks;
-    // }else{
-    //   this.tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-    //   return this.tasks;
-    // }
    }
+
    addtask(task: Task){
     this.tasks.push(task);
-    //  let tasks: Task[]= [];
-    // if(localStorage.getItem('tasks') === null){
-    //   tasks.push(task);
-    //   localStorage.setItem('task', JSON.stringify(tasks));
-    // }else{
-    //   task = JSON.parse(localStorage.getItem('tasks')|| '[]');
-    //   tasks.push(task);
-    //   localStorage.setItem('tasks', JSON.stringify(tasks));
-    // }
    }
+
    deleteTask(task: Task){
     for(let i = 0; i < this.tasks.length; i++){
       if(task == this.tasks[i]){
