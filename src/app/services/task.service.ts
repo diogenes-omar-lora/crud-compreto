afd77cb84b0a5939b91112fe8990d34fb235940d
diogenes-omar-lora <omarlora08@hotmail.com>
2022-11-28 22:27:50 -0400

    localstorage

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,25 +4,40 @@ import { Task} from '../models/task'
   providedIn: 'root'
 })
 export class TaskService {
-tasks: Task[];
+tasks: Task[] = [];
   constructor() {
-    this.tasks =[
-      {title:'Welcome',description:'This is My Project', hide:true}
-    ];
+    // this.tasks =[
+    //   {title:'Welcome',description:'This is My Project', hide:true}
+    // ];
    }
-   
    gettasks(){
-    return this.tasks;
+    if(localStorage.getItem('tasks')===null){
+      return this.tasks;
+    }else{
+      this.tasks = JSON.parse(localStorage.getItem('tasks')!);
+      return this.tasks;
+    }
+
    }
 
    addtask(task: Task){
     this.tasks.push(task);
+    let tasks:Task [] = [];
+    if(localStorage.getItem('tasks') === null){
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }else{
+      tasks = JSON.parse(localStorage.getItem('tasks')!);
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
    }
 
    deleteTask(task: Task){
     for(let i = 0; i < this.tasks.length; i++){
       if(task == this.tasks[i]){
         this.tasks.splice(i,1);
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
       }
     }
    }
